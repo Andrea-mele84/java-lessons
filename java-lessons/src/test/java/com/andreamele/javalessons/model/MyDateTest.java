4818c9c9e0505884d88a23a9aac202327367395c
e-andrea.mele <e-andrea.mele@wolterskluwer.com>
2024-11-29 17:48:35 +0100

    aggiunta di Test alla classe MyDate e correzzione Test contruttore un argomento

--- java-lessons/src/test/java/com/andreamele/javalessons/model/MyDateTest.java
+++ java-lessons/src/test/java/com/andreamele/javalessons/model/MyDateTest.java
@@ -2,43 +2,79 @@ package com.andreamele.javalessons.model;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
 
 class MyDateTest {
 
     @Test
-    void checkValidDayAndMonthOkWithFebruaryTest(){
-        int day = 29;
+    void checkValidDayAndMonthOkWithFebruaryTest() {
+        int day = 20;
         int month = 2;
         MyDate dayAndMonthOk = new MyDate(day, month, 2024);
         Assertions.assertDoesNotThrow(() -> dayAndMonthOk.checkValidDayAndMonth(day, month));
     }
 
     @Test
-    void checkValidDayAndMonthKoTest(){
+    void checkValidDayAndMonthKoTest() {
         int day = 30;
         int month = 2;
-        MyDate dayAndMonthKo = new MyDate (day, month, 2024);
+        MyDate dayAndMonthKo = new MyDate(day, month, 2024);
         Assertions.assertThrows(IllegalArgumentException.class, () -> dayAndMonthKo.checkValidDayAndMonth(day, month));
     }
 
     @Test
-    void checkValidDayAndMonthOkWithNotFebruaryTest(){
-        int day = 30;
+    void checkValidDayAndMonthOkWithNotFebruaryTest() {
+        int day = 25;
         int month = 1;
-        MyDate dateNotFebruary = new MyDate(day , month, 2024);
+        MyDate dateNotFebruary = new MyDate(day, month, 2024);
         Assertions.assertDoesNotThrow(() -> dateNotFebruary.checkValidDayAndMonth(day, month));
     }
 
     @Test
-    void checkMyDateConstructorWithTwoArgsOKTest(){
+    void checkMyDateConstructorWithTwoArgsOKTest() {
         int month = 7;
         int year = 2020;
-        int expectedDay =1;
+        int expectedDay = 1;
         //Assertions.assertDoesNotThrow(() -> new MyDate(month, year));
-        MyDate dateOk = new MyDate(month, year);
-        Assertions.assertEquals(expectedDay, dateOk.getDay());
+        MyDate dateTwoArgsOk = new MyDate(month, year);
+        Assertions.assertEquals(expectedDay, dateTwoArgsOk.getDay());
 
     }
-    //per casa testare il ko del checkmydatetwoarguments sopra
-    //testare il cstruttore che prende solo anno
+
+    //per casa testare il ko del checkmydatetwoarguments
+    @Test
+    void checkMyDateConstructorWithTwoArgsKOTest() {
+        int month = 7;
+        int year = 2020;
+        int expectedDay = 5;
+        MyDate dateKO = new MyDate(month, year);
+        Assertions.assertNotEquals(expectedDay, dateKO.getDay());
+
+    }
+    //testare il costruttore che prende solo anno
+
+    @Test
+    void checkMyDateConstructorWithOneArgOKTest() {
+        int expectedDay = 1;
+        int expectedMonth = 1;
+        int year = 2024;
+        MyDate oneArg = new MyDate(year);
+        Assertions.assertEquals(expectedDay, expectedMonth, oneArg.getYear());
+        //Assertions.assertEquals(year, oneArg.getYear());
+
+    }
+
+    @Test
+    void checkMyDateConstructorWithOneArgKOTest() {
+        int expectedDay = 5;
+        int expectedMonth = 5;
+        int year = 2024;
+        MyDate oneArg = new MyDate(year);
+        Assertions.assertNotEquals(expectedDay,expectedMonth oneArg.getYear());
+
+    }
 }
+
+
+
+
